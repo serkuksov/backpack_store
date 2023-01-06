e02e7973710ed13fff7482d358c30030f6cc0750
Sergei Kuksov <ser.kuksov@mail.ru>
2023-01-06 19:34:47 +0300

    Доступ к картинкам перенесен в шаблон

--- products/views.py
+++ products/views.py
@@ -8,21 +8,13 @@ def detail(request, pk=None):
                select_related('brand', 'category').
                prefetch_related(Prefetch('images', queryset=Image.objects.all())).
                first())
-    qs = (Product.objects.
-          prefetch_related(Prefetch('images', queryset=Image.objects.all())).
-          order_by('?')[:4])
-    products = []
-    for elm in qs:
-        products.append({
-            'id': elm.id,
-            'name': elm.name,
-            'price': elm.price,
-            'url_img': elm.images.all()[0].image.url,
-        })
+    additional_products = (Product.objects.
+                           prefetch_related(Prefetch('images', queryset=Image.objects.all())).
+                           # only('name', 'price').
+                           order_by('?')[:4])
     context = {
         'product': product,
         'title': product.name,
-        'images': product.images.all(),
-        'products': products,
+        'additional_products': additional_products,
     }
     return render(request, 'products/detail.html', context)
