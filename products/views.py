20cb32cf2fcdd7210973009fdfd3a2a71dfd103c
Sergei Kuksov <ser.kuksov@mail.ru>
2023-01-18 22:00:34 +0300

    Сортировка каталога

--- products/views.py
+++ products/views.py
@@ -56,6 +56,20 @@ class ProductsView(ListView):
     template_name = 'products/catalog.html'
     paginate_by = 9
 
+    def get_ordering(self):
+        sort = self.request.GET.getlist('sort')
+        print(sort)
+        if sort:
+            sort = sort[-1]
+            if sort == '1':
+                return '-id'
+            elif sort == '2':
+                return 'price'
+            elif sort == '3':
+                return '-price'
+        else:
+            return 'id'
+
     def get_queryset(self):
         queryset = Product.objects
         category_id = self.request.GET.getlist('category')
@@ -75,21 +89,58 @@ class ProductsView(ListView):
         brand_id = self.request.GET.getlist('brand')
         if brand_id:
             queryset = queryset.filter(brand__in=brand_id)
+        ordering = self.get_ordering()
+        print(ordering)
         queryset = (queryset.
                     prefetch_related(Prefetch('images', queryset=Image.objects.all())).
-                    order_by('id'))
+                    order_by(ordering))
         return queryset
+
+    def get_sorts(self):
+        sort = self.request.GET.getlist('sort')
+        print(sort)
+        if sort:
+            sort = sort[-1]
+            if sort == '1':
+                return [
+                    ['По новизне', 1],
+                    ['По популярности', 0],
+                    ['По возрастанию цены', 2],
+                    ['По убыванию цены', 3],
+                ]
+            elif sort == '2':
+                return [
+                    ['По возрастанию цены', 2],
+                    ['По популярности', 0],
+                    ['По новизне', 1],
+                    ['По убыванию цены', 3],
+                ]
+            elif sort == '3':
+                return [
+                    ['По убыванию цены', 3],
+                    ['По популярности', 0],
+                    ['По новизне', 1],
+                    ['По возрастанию цены', 2],
+                ]
+        else:
+            return [
+                ['По популярности', 0],
+                ['По новизне', 1],
+                ['По возрастанию цены', 2],
+                ['По убыванию цены', 3],
+            ]
 
     def get_context_data(self, *args, object_list=None, **kwargs):
         context = super().get_context_data(*args, object_list=None, **kwargs)
         # categories = Category.objects.all()
         brands = Product.objects.values('brand__name', 'brand_id').annotate(total=Count('id'))
-        get_params = ''.join([f'{key}={value}&' for key, value in self.request.GET.items() if key != 'page'])
-        print(self.request.GET)
+        get_params = ''.join(
+            [f'{key}={value}&' for key, value in self.request.GET.items() if key != 'page' and key != 'sort'])
         context = context | {
             'title': 'Каталог',
             # 'categories': categories,
             'brands': brands,
             'get_params': get_params,
+            'sorts': self.get_sorts()
         }
         return context
