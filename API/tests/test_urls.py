7c50942ba17110c72203080e49647c79ef824b3c
Sergei Kuksov <ser.kuksov@mail.ru>
2023-03-04 18:03:16 +0300

    Unittest для API проверка urls

---
+++ API/tests/test_urls.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from rest_framework import status
+
+from API import views
+
+
+class UrlsTestCase(TestCase):
+    """Тесты для urls"""
+
+    def test_products_url_resolves(self):
+        url = reverse('API:products')
+        self.assertEquals(resolve(url).func.cls, views.ProductListAPIView)
+
+    def test_brands_url_resolves(self):
+        url = reverse('API:brands')
+        self.assertEquals(resolve(url).func.cls, views.BrandListView)
+
+    def test_carts_url_resolves(self):
+        url = reverse('API:cart-list')
+        self.assertEqual(resolve(url).func.cls, views.CartViewSet)
+        url = reverse('API:cart-detail', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.cls, views.CartViewSet)
